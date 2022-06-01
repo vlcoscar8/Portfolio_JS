af01b06f0fe6527427c8e90b405eac466f598d15
vlcoscar8 <vlcoscar8@gmail.com>
2022-06-01 18:33:34 +0200

    data of the projects stored in data folder

---
+++ src/app/data/projects.data.js
@@ -0,0 +1,165 @@
+export const projectsData = [
+    {
+        id: 1,
+        title: "Job finder web",
+        description:
+            "A web where you can find the last job offers from Remotive Api. With a filter options, pagination and list of favourites jobs.",
+        stack: [
+            {
+                name: "Javascript",
+                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png",
+            },
+            {
+                name: "Sass",
+                img: "https://cdn.iconscout.com/icon/free/png-256/sass-226054.png",
+            },
+        ],
+        pictures: [
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654077750/RecipeAssets/FoodCategory/fob_finder_app_olkx9y.png",
+            },
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101019/RecipeAssets/FoodCategory/fob_finder_app_01_murdjk.png",
+            },
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101019/RecipeAssets/FoodCategory/fob_finder_app_02_t2rxfr.png",
+            },
+        ],
+        code: [
+            {
+                description:
+                    "The code below describe the way how the data is printed on the view",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101018/RecipeAssets/FoodCategory/fob_finder_app_code01_gfzyzg.png",
+            },
+            {
+                description:
+                    "How the data from the Remotive Api is getted and used",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101018/RecipeAssets/FoodCategory/fob_finder_app_code02_bxo1em.png",
+            },
+            {
+                description:
+                    "In this case, the code below describe how the favourite list is managed",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101018/RecipeAssets/FoodCategory/fob_finder_app_code03_ggleaq.png",
+            },
+            {
+                description:
+                    "Last picture is about the OOP application to print the companies data",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101018/RecipeAssets/FoodCategory/fob_finder_app_code04_epa8fa.png",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Sneakers e-commerce",
+        description:
+            "An e-commerce about sneakers where the you can registered to buy a shoe adding to cart and a personal account view where the user can check the products bought and manage the personal information. This project is an start-to-end project with not only frontend development, but also backend with an APi to manage all the data used in the application.",
+        stack: [
+            {
+                name: "Angular",
+                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png",
+            },
+            {
+                name: "Sass",
+                img: "https://cdn.iconscout.com/icon/free/png-256/sass-226054.png",
+            },
+            {
+                name: "NodeJs",
+                img: "https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png",
+            },
+            {
+                name: "Mongo",
+                img: "https://img.icons8.com/color/452/mongodb.png",
+            },
+        ],
+        pictures: [
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654077754/RecipeAssets/FoodCategory/sneakers_ecommerce_lla1yx.png",
+            },
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101075/RecipeAssets/FoodCategory/sneakers_ecommerce_01_b7b6jc.png",
+            },
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101075/RecipeAssets/FoodCategory/sneakers_ecommerce_02_e1y7k6.png",
+            },
+        ],
+        code: [
+            {
+                description:
+                    "The code below is the product component used in several views of the page",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101075/RecipeAssets/FoodCategory/sneakers_ecommerce_code_01_l9pwzt.png",
+            },
+            {
+                description:
+                    "The subscribe methods created to manage the information that comes from the api",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101075/RecipeAssets/FoodCategory/sneakers_ecommerce_code_02_oxa6hk.png",
+            },
+            {
+                description:
+                    "The pipes created to manage the list of products and filter by several manners",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101075/RecipeAssets/FoodCategory/sneakers_ecommerce_code_03_g7nixp.png",
+            },
+            {
+                description:
+                    "The services created to set and get the data and share with differents components",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101075/RecipeAssets/FoodCategory/sneakers_ecommerce_code_04_yfqoa8.png",
+            },
+            {
+                description:
+                    "A switch map function to create a promise all with forkjoin to get the list of products that the user has bought",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101075/RecipeAssets/FoodCategory/sneakers_ecommerce_code_05_cid1y9.png",
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "Recipe social media app",
+        description:
+            "A recipe social media web, where the users can create their own recipes and post on the web, a personal account view where the user can update his personal info and also a backend api created with all the endpoints to get/post and delete the info.",
+        stack: [
+            {
+                name: "Javascript",
+                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png",
+            },
+            {
+                name: "Sass",
+                img: "https://cdn.iconscout.com/icon/free/png-256/sass-226054.png",
+            },
+            {
+                name: "NodeJs",
+                img: "https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png",
+            },
+            {
+                name: "Mongo",
+                img: "https://img.icons8.com/color/452/mongodb.png",
+            },
+        ],
+        pictures: [
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654086321/RecipeAssets/FoodCategory/recipe_o4yatx.png",
+            },
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101148/RecipeAssets/FoodCategory/recipe_01_dct6qs.png",
+            },
+            {
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101148/RecipeAssets/FoodCategory/recipe_02_rq6bpf.png",
+            },
+        ],
+        code: [
+            {
+                description:
+                    "A picture about how the form data is managed to update the personal information in the personal account view",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101147/RecipeAssets/FoodCategory/recipe_code-01_vtclwx.png",
+            },
+            {
+                description:
+                    "The code below shows how the data is managed from the frontend to get the data from the backend",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101147/RecipeAssets/FoodCategory/recipe_code-02_dntxmm.png",
+            },
+            {
+                description:
+                    "In this case the picture show the code about how the data is printed to the view",
+                img: "https://res.cloudinary.com/oscar-perez/image/upload/v1654101147/RecipeAssets/FoodCategory/recipe_code-03_rtxea3.png",
+            },
+        ],
+    },
+];
