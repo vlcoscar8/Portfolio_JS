0f55648f4400bdcbb60677c0bbdc2f1718d7d0a5
vlcoscar8 <vlcoscar8@gmail.com>
2022-06-01 18:35:39 +0200

    filter to get the project depending on where the user click on the project list

--- src/app/controller/printDetailProject.js
+++ src/app/controller/printDetailProject.js
@@ -1,5 +1,8 @@
+import { projectsData } from "../data/projects.data.js";
+
 const printDetailProject = (id) => {
-    console.log(id);
+    const projectData = projectsData.filter((project) => project.id === id);
+    console.log(projectData);
 };
 
 export { printDetailProject };
