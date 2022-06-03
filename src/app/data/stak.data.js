c351996d0d32cdae7be62328a2aa9417ee4453f0
vlcoscar8 <vlcoscar8@gmail.com>
2022-06-03 15:52:28 +0200

    stack info updated with map function

---
+++ src/app/data/stak.data.js
@@ -0,0 +1,42 @@
+export const stack = [
+    {
+        name: "Html",
+        img: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+    },
+    {
+        name: "Css",
+        img: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+    },
+    {
+        name: "Sass",
+        img: "https://cdn.iconscout.com/icon/free/png-256/sass-226054.png",
+    },
+    {
+        name: "Javascript",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png",
+    },
+    {
+        name: "Angular",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png",
+    },
+    {
+        name: "Angular",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png",
+    },
+    {
+        name: "React",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+    },
+    {
+        name: "NodeJs",
+        img: "https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png",
+    },
+    {
+        name: "Mongo",
+        img: "https://img.icons8.com/color/452/mongodb.png",
+    },
+    {
+        name: "Git",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1024px-Git_icon.svg.png",
+    },
+];
